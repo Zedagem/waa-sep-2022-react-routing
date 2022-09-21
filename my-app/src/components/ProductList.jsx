0d0186ae46f5005d101f941780973e2c0046584a
Zedagem <zedshif123@gmail.com>
2022-09-21 16:54:26 -0500

    product can only be accessed by logged in users

--- my-app/src/components/ProductList.jsx
+++ my-app/src/components/ProductList.jsx
@@ -1,16 +1,26 @@
 import { useEffect, useState } from "react";
 import Product from "./Product";
 import { instance } from "../index";
+import { useNavigate } from "react-router-dom";
+
+
 export default function(props){
-    const [productData,setProductData] = useState([])
+
+    const [productData,setProductData] = useState([]);
+    const navigator = useNavigate();
+
     useEffect(()=>{
         const token = JSON.parse(localStorage.getItem('token'))
         instance.get('/products',{
             headers:{
-                "Authorization":`Bearer ${token.accessToken}`
+                "Authorization":`Bearer ${token ? token.accessToken : ''}`
             }
         }).then(response => setProductData(response.data))
-        .catch(err=>console.log(err))
+        .catch(err=>{
+            console.log(err)
+            navigator('/')
+
+        })
     },[])
     return(
         <div>
