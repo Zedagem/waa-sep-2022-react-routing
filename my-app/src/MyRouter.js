e50d7fd6ce0b190d8e579bb519ae192cc014de13
Zedagem <zedshif123@gmail.com>
2022-09-21 17:08:01 -0500

    Page not found added

--- my-app/src/MyRouter.js
+++ my-app/src/MyRouter.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import ProductList from "./components/ProductList";
 import SignupForm from "./components/SignupForm";
+import PageNotFound from "./PageNotFound";
 export default function () {
   return (
     <div>
@@ -9,6 +10,7 @@ export default function () {
         <Route path="/" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/products" element={<ProductList/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
       </Routes>
     </div>
   );
